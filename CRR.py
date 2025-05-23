15626f7405abc1dc20307ef7d211ecf58d91dfd4
andres.cruz <andres.cruz@ine.mx>
2025-05-22 22:34:15 -0600

    Agrega implementación de CRR forward

--- CRR.py
+++ CRR.py
@@ -1,4 +1,6 @@
 from bigtree import Node, tree_to_dot
+import numpy as np
+import matplotlib.pyplot as plt
 
 class CRR:
 
@@ -32,9 +34,32 @@ class CRR:
         #print('valor en la iteracion: ' + str(valor_a))
         return nodo, valor_a
 
+    def construye_trayectorias(self,n,t):
+        return np.random.randint(2, size=(n, t))
+
+    def ejecuta_crr(self):
+        n = 5 #Numero de trayectorias
+        t = 10 #Numero de intervalos
+        trayectorias = self.construye_trayectorias(n,t)
+        S_0 = 10 #Precio base del activo
+        matriz_inicial = np.zeros((n,t+1))
+        matriz_inicial[:,0]= np.full((1,n),S_0)
+        print(matriz_inicial)
+        print(trayectorias)
+        for col in range(trayectorias.shape[1]):
+            matriz_inicial[:,col+1] = np.multiply(matriz_inicial[:,col],(self.b)*trayectorias[:,col]) + np.multiply(matriz_inicial[:,col],(self.a)*(1-trayectorias[:,col]))
+        for i in range(matriz_inicial.shape[0]):
+            plt.plot(matriz_inicial[i], label=f'Trayectoria {i + 1}')
+
+        plt.xlabel('t')
+        plt.ylabel('Value')
+        plt.title('Distintas trayectorias ')
+        plt.legend()
+        plt.show()
+
 if __name__  == '__main__':
     a = 0.5 #Valor de bajada
-    b = 0.75 #Valor de subida
+    b = 1.75 #Valor de subida
     p = .4 #Probabilidad de a
     T = 5 #Iteraciones
     c1 = CRR(a,b,p,T)
@@ -42,3 +67,5 @@ if __name__  == '__main__':
     #graph = tree_to_dot(arbol, node_colour="gold") #<--- activar este para generar la imagen que se ve en tree.png, con más de 5 ciclos, se atora
     #graph.write_png("tree.png")
     print('Calculando CRR: '+ str(valor))
+
+    c1.ejecuta_crr()
